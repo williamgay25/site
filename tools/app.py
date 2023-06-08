fb92c936749b68b9d21f5073638afacfaf7508cc
William Gay <williamgay360@gmail.com>
2023-06-08 12:34:30 -0400

    chore: updating url passing

--- tools/app.py
+++ tools/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, send_file
 from tools import qr
 
@@ -7,10 +8,15 @@ app = Flask(__name__)
 def qrcode_generator():
     if request.method == 'POST':
         text = request.form['website_name']
-        qr.generate_qr_code(text)
-        qrcode_url = url_for('static', filename='qrcode.png')
+        filename = qr.generate_qr_code(text)
+        qrcode_url = url_for('serve_qrcode', filename=filename)
         return render_template('index.html', qrcode=qrcode_url)
     return render_template('index.html')
+
+@app.route('/qrcode/<path:filename>')
+def serve_qrcode(filename):
+    tmp_folder = 'tmp' 
+    return send_file(os.path.join(tmp_folder, filename), mimetype='image/png')
 
 if __name__ == '__main__':
     app.run()
