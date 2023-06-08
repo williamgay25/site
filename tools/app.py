5fda25c76b53fc178d8123ec3a36d0e87d050ee0
William Gay <williamgay360@gmail.com>
2023-06-08 12:53:03 -0400

    chore: overhaul file structure

--- tools/app.py
+++ tools/app.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_file
-from tools import qr
+from flask import Flask, render_template, request, send_file
+from tools import Tools
 
 app = Flask(__name__)
 
@@ -8,15 +8,14 @@ app = Flask(__name__)
 def qrcode_generator():
     if request.method == 'POST':
         text = request.form['website_name']
-        filename = qr.generate_qr_code(text)
-        qrcode_url = url_for('serve_qrcode', filename=filename)
-        return render_template('index.html', qrcode=qrcode_url)
+        path = Tools.generate_qr_code(text)
+        qrcode_path = os.path.join('tmp', path)
+        return render_template('index.html', qrcode=qrcode_path)
     return render_template('index.html')
 
-@app.route('/qrcode/<path:filename>')
+@app.route('/tmp/<path:filename>')
 def serve_qrcode(filename):
-    tmp_folder = 'tmp' 
-    return send_file(os.path.join(tmp_folder, filename), mimetype='image/png')
+    return send_file(os.path.join('tmp', filename), mimetype='image/png')
 
 if __name__ == '__main__':
     app.run()
