8eaabb4161e4760aac74ddf612d82cd09a851950
William Gay <williamgay360@gmail.com>
2023-11-28 21:10:00 -0500

    chore: link cleanup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,8 +22,8 @@ function Home() {
           <h2>wannabe founder</h2>
           <ul>
             <li><a href="https://tempus-ex.com">tempus ex machina</a> - worked on a fan engagement startup with the nfl. wrote some react native.</li>
-            <li><a href="https://findflayr.com">flayrai</a> - capstone project turned startup accelerator. worked on restaurant hiring and learned a lot.</li>
-            <li><a href="https://iterate.tools">iterate.tools</a> - building an AI-powered website builder. sign up today!</li>
+            <li><a href="https://github.com/flayrai">flayrai</a> - capstone project turned startup accelerator. worked on restaurant hiring and learned a lot.</li>
+            <li><a href="https://github.com/iteratedottools">iterate.tools</a> - building an AI-powered website builder. sign up today!</li>
           </ul>
         </section>
 
@@ -32,7 +32,6 @@ function Home() {
           <ul>
             <li><a href="https://github.com/williamgay25/micrograd">micrograd</a> - building a neural net library from scratch.</li>
             <li><a href="https://github.com/williamgay25/makemore">makemore</a> - working on a character level language model.</li>
-            <li><a href="https://github.com/williamgay25/toolbox">toolbox</a> - building a hub for tools i use too often.</li>
             <li><a href="https://github.com/williamgay25/crawler">crawler</a> - basic python web crawler.</li>
           </ul>
         </section>
